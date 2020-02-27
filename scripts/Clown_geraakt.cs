fbfcda499358a1f9e6fcc7be0ed31d0fe4795023
Merel-04 <merelw@hotmail.com>
2020-02-27 11:56:34 +0100

    Current scripts in my project from week 3

---
+++ scripts/Clown_geraakt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clown_geraakt : MonoBehaviour
+{
+    public AudioSource splatter_source;
+    void OnCollisionEnter(Collision CollisionInfo) {
+
+        if (CollisionInfo.collider.name == "CakePrefab(Clone)") {
+            splatter_source.Play();
+        }
+
+    }
+
+}
