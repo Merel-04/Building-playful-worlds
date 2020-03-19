c21d967847fb49152cae4f1b339140957a986675
Merel-04 <merelw@hotmail.com>
2020-03-19 21:54:24 +0100

    a lot has changed

---
+++ Buiding playful Worlds/Assets/scripts/Go_to_cutscene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Go_to_cutscene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            SceneManager.LoadScene("Cutscene");
+    }
+}
